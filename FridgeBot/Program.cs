c493221cb3322e2ccdf5332ea367babe3a8f88b1
Dirk Kok <the@dirkkok.nl>
2023-04-05 00:45:29 +0200

    top level program

--- FridgeBot/Program.cs
+++ FridgeBot/Program.cs
@@ -5,6 +5,7 @@ using DSharpPlus.Entities;
 using DSharpPlus.Exceptions;
 using Foxite.Common;
 using Foxite.Common.Notifications;
+using FridgeBot;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -12,129 +13,120 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Qmmands;
 
-namespace FridgeBot; 
+using IHost host = Host.CreateDefaultBuilder(args)
+	.ConfigureAppConfiguration((hostingContext, configuration) => {
+		configuration.Sources.Clear();
 
-public sealed class Program {
-	private static IHostBuilder CreateHostBuilder(string[] args) =>
-		Host.CreateDefaultBuilder(args)
-			.ConfigureAppConfiguration((hostingContext, configuration) => {
-				configuration.Sources.Clear();
-
-				configuration
-					.AddJsonFile("appsettings.json", true, true)
-					.AddJsonFile($"appsettings.{hostingContext.HostingEnvironment.EnvironmentName}.json", true, true)
-					.AddEnvironmentVariables("FRIDGE_")
-					.AddCommandLine(args);
-			});
-
-	private static async Task Main(string[] args) {
-		using IHost host = CreateHostBuilder(args)
-			.ConfigureLogging((_, builder) => {
-				builder.AddExceptionDemystifyer();
-			})
-			.ConfigureServices((hbc, isc) => {
-				//isc.Configure<DiscordConfiguration>(hbc.Configuration.GetSection("Discord"));
-				isc.Configure<ConnectionStringsConfiguration>(hbc.Configuration.GetSection("ConnectionStrings"));
-					
-				isc.AddSingleton(isp => {
-					var config = new DiscordConfiguration {
-						Token = hbc.Configuration.GetSection("Discord").GetValue<string>("Token"),
-						Intents = DiscordIntents.GuildMessages | DiscordIntents.GuildMembers | DiscordIntents.GuildMessageReactions | DiscordIntents.Guilds,
-						LoggerFactory = isp.GetRequiredService<ILoggerFactory>(),
-						MinimumLogLevel = LogLevel.Information,
-						MessageCacheSize = 0
-					};
-					return new DiscordClient(config);
-				});
-
-				isc.AddSingleton<HttpClient>();
-				isc.AddScoped<FridgeService>();
-				isc.AddSingleton<IFridgeTarget, DiscordFridgeTarget>();
-					
-				isc.ConfigureDbContext<FridgeDbContext>();
-					
-				isc.AddNotifications().AddDiscord(hbc.Configuration.GetSection("DiscordNotifications"));
-
-				isc.AddSingleton<CommandService>();
-			})
-			.Build();
+		configuration
+			.AddJsonFile("appsettings.json", true, true)
+			.AddJsonFile($"appsettings.{hostingContext.HostingEnvironment.EnvironmentName}.json", true, true)
+			.AddEnvironmentVariables("FRIDGE_")
+			.AddCommandLine(args);
+	})
+	.ConfigureLogging((_, builder) => {
+		builder.AddExceptionDemystifyer();
+	})
+	.ConfigureServices((hbc, isc) => {
+		//isc.Configure<DiscordConfiguration>(hbc.Configuration.GetSection("Discord"));
+		isc.Configure<ConnectionStringsConfiguration>(hbc.Configuration.GetSection("ConnectionStrings"));
 			
-		await using (var dbContext = host.Services.GetRequiredService<FridgeDbContext>()) {
-			await dbContext.Database.MigrateAsync();
-		}
+		isc.AddSingleton(isp => {
+			var config = new DiscordConfiguration {
+				Token = hbc.Configuration.GetSection("Discord").GetValue<string>("Token"),
+				Intents = DiscordIntents.GuildMessages | DiscordIntents.GuildMembers | DiscordIntents.GuildMessageReactions | DiscordIntents.Guilds,
+				LoggerFactory = isp.GetRequiredService<ILoggerFactory>(),
+				MinimumLogLevel = LogLevel.Information,
+				MessageCacheSize = 0
+			};
+			return new DiscordClient(config);
+		});
 
-		var logger = host.Services.GetRequiredService<ILogger<Program>>();
-		var notifications = host.Services.GetRequiredService<NotificationService>();
-		var discord = host.Services.GetRequiredService<DiscordClient>();
+		isc.AddSingleton<HttpClient>();
+		isc.AddScoped<FridgeService>();
+		isc.AddSingleton<IFridgeTarget, DiscordFridgeTarget>();
 			
-		async Task HandleHandlerException(string name, Exception exception, DiscordMessage? message) {
-			string N(object? o) => o?.ToString() ?? "null";
-			FormattableString errorMessage =
-				@$"Exception in {name}
-					   message: {N(message?.Id)} ({N(message?.JumpLink)}), type: {N(message?.MessageType?.ToString() ?? "(null)")}, webhook: {N(message?.WebhookMessage)}
-					   author: {N(message?.Author?.Id)} ({N(message?.Author?.Username)}#{N(message?.Author?.Discriminator)}), bot: {N(message?.Author?.IsBot)}
-					   channel {N(message?.Channel?.Id)} ({N(message?.Channel?.Name)})
-					   {(message?.Channel?.Guild != null ? $"guild {N(message?.Channel?.Guild?.Id)} ({N(message?.Channel?.Guild?.Name)})" : "")}";
-			logger.LogCritical(exception, errorMessage);
-			await notifications.SendNotificationAsync(errorMessage, exception.Demystify());
-		}
+		isc.ConfigureDbContext<FridgeDbContext>();
+			
+		isc.AddNotifications().AddDiscord(hbc.Configuration.GetSection("DiscordNotifications"));
 
-		var commands = host.Services.GetRequiredService<CommandService>();
-		commands.AddModules(Assembly.GetExecutingAssembly());
-		commands.AddTypeParser(new DiscordEmojiParser());
-		commands.AddTypeParser(new DiscordChannelParser());
+		isc.AddSingleton<CommandService>();
+	})
+	.Build();
+	
+await using (var dbContext = host.Services.GetRequiredService<FridgeDbContext>()) {
+	await dbContext.Database.MigrateAsync();
+}
 
-		discord.MessageCreated += async (sender, eventArgs) => {
-			if (eventArgs.Message.Content.StartsWith(discord.CurrentUser.Mention)) {
-				IResult result = await commands.ExecuteAsync(eventArgs.Message.Content.Substring(discord.CurrentUser.Mention.Length), new DSharpPlusCommandContext(eventArgs.Message, host.Services));
-				if (result is not SuccessfulResult) {
-					string? message;
-					if (result is ChecksFailedResult cfr) {
-						message = string.Join("\n", cfr.FailedChecks.Select(tuple => tuple.Result).Where(cr => !cr.IsSuccessful).Select(cr => $"- {cr.FailureReason}"));
-					} else {
-						message = result.ToString();
-					}
-						
-					await eventArgs.Message.RespondAsync(message);
-				}
+var logger = host.Services.GetRequiredService<ILogger<Program>>();
+var notifications = host.Services.GetRequiredService<NotificationService>();
+var discord = host.Services.GetRequiredService<DiscordClient>();
+	
+async Task HandleHandlerException(string name, Exception exception, DiscordMessage? message) {
+	string N(object? o) => o?.ToString() ?? "null";
+	FormattableString errorMessage =
+		@$"Exception in {name}
+			   message: {N(message?.Id)} ({N(message?.JumpLink)}), type: {N(message?.MessageType?.ToString() ?? "(null)")}, webhook: {N(message?.WebhookMessage)}
+			   author: {N(message?.Author?.Id)} ({N(message?.Author?.Username)}#{N(message?.Author?.Discriminator)}), bot: {N(message?.Author?.IsBot)}
+			   channel {N(message?.Channel?.Id)} ({N(message?.Channel?.Name)})
+			   {(message?.Channel?.Guild != null ? $"guild {N(message?.Channel?.Guild?.Id)} ({N(message?.Channel?.Guild?.Name)})" : "")}";
+	logger.LogCritical(exception, errorMessage);
+	await notifications.SendNotificationAsync(errorMessage, exception.Demystify());
+}
+
+var commands = host.Services.GetRequiredService<CommandService>();
+commands.AddModules(Assembly.GetExecutingAssembly());
+commands.AddTypeParser(new DiscordEmojiParser());
+commands.AddTypeParser(new DiscordChannelParser());
+
+discord.MessageCreated += async (sender, eventArgs) => {
+	if (eventArgs.Message.Content.StartsWith(discord.CurrentUser.Mention)) {
+		IResult result = await commands.ExecuteAsync(eventArgs.Message.Content.Substring(discord.CurrentUser.Mention.Length), new DSharpPlusCommandContext(eventArgs.Message, host.Services));
+		if (result is not SuccessfulResult) {
+			string? message;
+			if (result is ChecksFailedResult cfr) {
+				message = string.Join("\n", cfr.FailedChecks.Select(tuple => tuple.Result).Where(cr => !cr.IsSuccessful).Select(cr => $"- {cr.FailureReason}"));
+			} else {
+				message = result.ToString();
 			}
-		};
+				
+			await eventArgs.Message.RespondAsync(message);
+		}
+	}
+};
 
-		async Task OnReactionModifiedAsync(DiscordMessage message, DiscordEmoji emoji, bool added) {
-			try {
-				// Acquire additional data such as the author, and refresh reaction counts
-				try {
-					message = await message.Channel.GetMessageAsync(message.Id);
-				} catch (NotFoundException) {
-					// Message was deleted since the event fired
-					return;
-				}
-				if (message == null) {
-					// Also deleted? idk. better safe than sorry
-					return;
-				}
-
-				await using var scope = host.Services.CreateAsyncScope();
-				var fridgeService = scope.ServiceProvider.GetRequiredService<FridgeService>();
-				await fridgeService.ProcessReactionAsync(new RealDiscordMessage(message));
-			} catch (Exception ex) {
-				await HandleHandlerException("OnReactionModifiedAsync", ex, message);
-			}
+async Task OnReactionModifiedAsync(DiscordMessage message, DiscordEmoji emoji, bool added) {
+	try {
+		// Acquire additional data such as the author, and refresh reaction counts
+		try {
+			message = await message.Channel.GetMessageAsync(message.Id);
+		} catch (NotFoundException) {
+			// Message was deleted since the event fired
+			return;
+		}
+		if (message == null) {
+			// Also deleted? idk. better safe than sorry
+			return;
 		}
 
-		discord.MessageReactionAdded += (sender, ea) => {
-			_ = OnReactionModifiedAsync(ea.Message, ea.Emoji, true);
-			return Task.CompletedTask;
-		};
-		discord.MessageReactionRemoved += (sender, ea) => {
-			_ = OnReactionModifiedAsync(ea.Message, ea.Emoji, false);
-			return Task.CompletedTask;
-		};
-
-		discord.ClientErrored += (_, eventArgs) => notifications.SendNotificationAsync($"Exception in {eventArgs.EventName}", eventArgs.Exception);
-			
-		await discord.ConnectAsync();
-
-		await host.RunAsync();
+		await using var scope = host.Services.CreateAsyncScope();
+		var fridgeService = scope.ServiceProvider.GetRequiredService<FridgeService>();
+		await fridgeService.ProcessReactionAsync(new RealDiscordMessage(message));
+	} catch (Exception ex) {
+		await HandleHandlerException("OnReactionModifiedAsync", ex, message);
 	}
 }
+
+discord.MessageReactionAdded += (sender, ea) => {
+	_ = OnReactionModifiedAsync(ea.Message, ea.Emoji, true);
+	return Task.CompletedTask;
+};
+discord.MessageReactionRemoved += (sender, ea) => {
+	_ = OnReactionModifiedAsync(ea.Message, ea.Emoji, false);
+	return Task.CompletedTask;
+};
+
+discord.ClientErrored += (_, eventArgs) => notifications.SendNotificationAsync($"Exception in {eventArgs.EventName}", eventArgs.Exception);
+	
+await discord.ConnectAsync();
+
+await host.RunAsync();
